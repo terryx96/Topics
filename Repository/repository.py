0da3e3b518a25d45667d86bae91e528a2de7c64b
Terry <terry.wade@valpo.edu>
2020-06-30 13:46:55 -0500

    Remove test method

--- Repository/repository.py
+++ Repository/repository.py
@@ -2,12 +2,6 @@ from Models.base import session_factory
 from Models.Course import Course
 from Models.Student import Student
 
-def get_all_courses():
-    session = session_factory()
-    courses_query = session.query(Course)
-    session.close()
-    return courses_query.all()
-
 def create_student(student):
     session = session_factory()
     session.add(student)
