eae730549c5cebf54dc1ce682620fc10f931894b
Terry <terry.wade@valpo.edu>
2020-06-30 13:11:16 -0500

    Add create and getStudentById methods

--- Repository/repository.py
+++ Repository/repository.py
@@ -1,5 +1,6 @@
 from Models.base import session_factory
 from Models.Course import Course
+from Models.Student import Student
 
 def get_all_courses():
     session = session_factory()
@@ -7,15 +8,17 @@ def get_all_courses():
     session.close()
     return courses_query.all()
 
-def create_student():
+def create_student(student):
     session = session_factory()
+    session.add(student)
+    session.commit()
     session.close()
-    return 
 
 def get_student_by_id(id):
     session = session_factory()
+    student = session.query(Student).get(id)
     session.close()
-    return 
+    return student 
 
 def update_student():
     session = session_factory()
