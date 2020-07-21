c4c4d81638efb4a8905a3860d834da1bc9d7e86d
Terry <terry.wade@valpo.edu>
2020-07-21 13:54:28 -0500

    Modulize service.py

--- Service/service.py
+++ Service/service.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-def import_data():
-    df = pd.read_csv("C:/Users/terra/Documents/Python/Topics/data.csv")
-    return df
+class Service():
+
+    courses = pd.DataFrame()
+    students = pd.DataFrame()
+
+    def import_data(self):
+        df = pd.read_csv("C:/Users/terra/Documents/Python/Topics/data.csv")
+        self.courses = df.iloc[:,0:7]
+        self.students = df.iloc[:,7:]
