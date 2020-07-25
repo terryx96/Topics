8604d544fe52189ed5bcc94cf2f4bffbbf4e8de7
Terry <36857016+terryx96@users.noreply.github.com>
2020-07-25 11:44:37 -0500

    Update service.py
    Hide personal filepath

--- Service/service.py
+++ Service/service.py
@@ -6,6 +6,6 @@ class Service():
     students = pd.DataFrame()
 
     def import_data(self):
-        df = pd.read_csv("C:/Users/terra/Documents/Python/Topics/data.csv")
+        df = pd.read_csv("")
         self.courses = df.iloc[:,0:7]
         self.students = df.iloc[:,7:]
