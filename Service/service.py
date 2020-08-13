606855a8e314376dd1de550a6deb10eaf74c0384
Terry <terry.wade@valpo.edu>
2020-08-13 14:59:52 -0500

    Update Github

--- Service/service.py
+++ Service/service.py
@@ -6,6 +6,6 @@ class Service():
     students = pd.DataFrame()
 
     def import_data(self):
-        df = pd.read_csv("C:/Users/terra/Documents/Python/Topics/data.csv")
+        df = pd.read_csv() # Will need to be a GUI function after flask implementation
         self.courses = df.iloc[:,0:7]
         self.students = df.iloc[:,7:]
